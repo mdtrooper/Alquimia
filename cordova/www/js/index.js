907e7380de7af4128527a89194ba95c54a174703
Miguel de Dios <tres.14159@gmail.com>
2019-04-17 03:28:48 +0200

    Added a tiny example to drag with interact.js .

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -15,6 +15,9 @@
     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 var app = {
+    stack_screens: ['main_menu'],
+    position: { x: 0, y: 0 },
+    
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -34,9 +37,83 @@ var app = {
             case 'deviceready':
                 //- INIT - Main menu ------------------------------------
                 $("#main_menu .btn_exit").on('click', function() {navigator.app.exitApp();});
+                $("#main_menu .btn_new_game").on('click', app.new_game);
+                $("#main_menu .btn_continue_game").on('click', app.continue_game);
+                $("#main_menu .btn_options").on('click', function() {app.go_screen('options'); });
                 //- END -- Main menu ------------------------------------
+                
+                
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrict({
+        restriction: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      }),
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
+    },
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+                
+                
                 break;
         }
+    },
+    
+    new_game: function() {
+        $('#alert_erase_game').modal('show')
+        //app.go_screen('game');
+    },
+    
+    continue_game: function() {
+        app.go_screen('game');
+    },
+    
+    go_screen: function(screen) {
+        $("#" + app.stack_screens[0]).addClass('hidden');
+        $("#" + screen).removeClass('hidden');
+        app.stack_screens.unshift(screen)
+    },
+    
+    add_test_other: function() {
+        $("#game").append($('<div class="draggable" style="background: yellow !important; top: 100px; left: 100px; position: absolute; width: 50px; height: 50px;">B</div>'));
     }
 };
 
